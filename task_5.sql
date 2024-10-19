940d44c07f841d2cfa57bd07b8c3af642d41c10a
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-10-19 02:26:37 -0700

    script that inserts a single row in the table customer (database alx_book_store) in your MySQL server.

--- task_5.sql
+++ task_5.sql
@@ -1,23 +1,23 @@
 #!bin/bash
 import mysql.connector
 
+
+
 connection = mysql.connector.connect(
     host = "localhost",
     user = "root",
     passwd = "Azegba1234567890@",
     database = "alx_book_store"
 )
-
-USE alx_book_store;
-
 cursor = connection.cursor()
+sql = "INSERT INTO TABLE Customer(customer_id,customer_name, email, address ) VALUES (%s,%s, %s,%s)"
+val = (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
 
-cursor.execute("SHOW TABLES")
+cursor.execute(sql, val)
 
-result = cursor.fetchall()
-for row in result:
-    print(row)
+connection.commit()
 
 connection.close()
 cursor.close()
+
 
