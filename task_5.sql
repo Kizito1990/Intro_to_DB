70c1410ca88bb9d9b43f4a7e88791d9953811d39
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-10-19 03:37:59 -0700

    Add file

--- task_5.sql
+++ task_5.sql
@@ -7,11 +7,22 @@ connection = mysql.connector.connect(
     host = "localhost",
     user = "root",
     passwd = "Azegba1234567890@",
-    database = "alx_book_store"
+
 )
+
 cursor = connection.cursor()
-sql = "INSERT INTO TABLE customer(customer_id,customer_name, email, address ) VALUES (%s,%s,%s)"
-val = ( "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
+USE alx_book_store;
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS customer (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL
+)
+""")
+
+sql = "INSERT INTO TABLE customer(customer_id,customer_name, email, address ) VALUES (%s,%s, %s, )"
+val = (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
 
 cursor.execute(sql, val)
 
@@ -19,5 +30,4 @@ connection.commit()
 
 connection.close()
 cursor.close()
-
 
